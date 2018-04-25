1a3857d8acd9d2cad69814b3a261d68fdaf46e47
Tino Kallinich <tkallinich@inovex.de>
2018-04-25 18:45:22 +0200

    recursion in python example fibonacci

--- functions.py
+++ functions.py
@@ -14,3 +14,21 @@ def some_funtion_with_param(param):
 
 some_function()
 some_funtion_with_param(10)
+
+# recursive function
+print("")
+print("define recursive functions in python")
+print("------------------------------------")
+
+def fib(n):
+    
+    result = 0
+    if n < 2:
+        result = n
+    else:
+        result =  fib(n - 1) + fib(n - 2)
+        
+    return result
+
+some_var = fib(10)
+print(some_var)
