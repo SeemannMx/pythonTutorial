f3afd43aa36b6fbf90bc070d11d540827ae4dc22
Tino Kallinich <tkallinich@inovex.de>
2018-04-25 14:38:48 +0200

    if / else elif requirements

--- hallo welt.py
+++ hallo welt.py
@@ -1,1 +1,27 @@
+print("")
 print("Hallo World")
+
+x = 42
+y = True
+
+if x < 52: 
+    print("if x: Hello World")
+    x = 52
+    
+if y: 
+    print("if y: Hello World")
+    y = False
+else:
+    print("else: exception")
+    
+if y: 
+    print("if y: Hello World")
+    y = False
+    x = 42
+elif x == 42:
+    print("elif: Antwort 42")
+else:
+    print("else: exception")
+
+print("")
+
