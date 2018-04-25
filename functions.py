1656e5dbda6c6fe6eb98655cec8e0181f97f3f97
Tino Kallinich <tkallinich@inovex.de>
2018-04-25 18:34:49 +0200

    define functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# define functions
+print("")
+print("define functions in python")
+print("--------------------------")
+# function / def is skipped until function call
+def some_function():
+    for i in range(0,20):
+        print("define a function")
+        
+print("")        
+def some_funtion_with_param(param):
+    for i in range(0 , param):
+        print("define a function with param")
+
+some_function()
+some_funtion_with_param(10)
---
+++ main_python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo start python tutorial script
+echo
+# make script excecutable
+chmod +x hallo\ welt.py
+chmod +x pythonTutorial.py
+chmod +x whileTest.py
+chmod +x breakpointTest.py
+chmod +x functions.py
+
+#execute scripts
+python3 hallo\ welt.py
+python3 pythonTutorial.py
+python3 whileTest.py
+python3 breakpointTest.py
+python3 functions.py
+
+echo
+echo  -e "\033[0;32;5mfinish with Tutorial\033[0m"
+ 
