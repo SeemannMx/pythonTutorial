db19a56a9c7b56603934110d539b40794ec04ae9
Tino Kallinich <tkallinich@inovex.de>
2018-04-25 14:00:25 +0200

    first commit

---
+++ pythonTutorial.py
@@ -0,0 +1,25 @@
+Zahlen
+9 // 2 	abrunden = 4
+2**3	potenzieren = 8
+
+Strings 
+gleiche zuweisung wie bei zahlen
+
+Ausgabe
+print("Hallo Welt") ausgabe in console
+
+Listen
+x = [1,2,3,4,5] 
+x[0] 	erstes element in der liste
+x[-1]	letzte element in einer liste
+
+x[0] = 10 zuweisung von element 10
+
+x + [6,7,8] liste zur liste hinzufügen
+
+x + [x[0]+x[1]] füllen vo listenelelement
+
+x.append(42) wird zu [-1, 2, 3, 4, 5, 42]
+
+abc = 42
+
