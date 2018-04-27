971ca97f7447130d97e7a308c8b166327037f32e
Tino Kallinich <tkallinich@inovex.de>
2018-04-27 17:29:51 +0200

    constructor with default value

--- classTest.py
+++ classTest.py
@@ -6,6 +6,12 @@ print("--------------------------")
 class TestClass:
     number = 15
     symbol = "symbol as a string"
+    
+    # constructor with default value
+    def __init__(self, x="constructor variable"):
+        self.something = x
+        print("Iam the constructor to TestClass\n ", self.something)
+    
     # defined function always with self access variables with self keyword
     def functionInClass(self, otherNumber):
         result = otherNumber + self.number
@@ -18,3 +24,6 @@ some_instance = TestClass()
 print("TestClass number:  ",some_instance.number)
 print("TestClass sysmbol: ",some_instance.symbol)
 some_instance.functionInClass(42)
+
+
+    
